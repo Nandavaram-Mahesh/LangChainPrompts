2d69e73652d2b219401f424456ca351ca4abe01b
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2025-08-04 04:39:07 +0530

    Chatbot- chat history functionality added

--- chatbot.py
+++ chatbot.py
@@ -16,16 +16,21 @@ llm = HuggingFaceEndpoint(
 
 model = ChatHuggingFace(llm=llm)
 
+chat_history = []
+
 
 while True:
     
     user_input = input("You: ")
-    
+    chat_history.append(user_input)
     if  user_input=='exit':
         break
     
-    response = model.invoke(user_input)
+    response = model.invoke(chat_history)
+    chat_history.append(response.content)
     
     print(f"Bot: {response.content}")
+
+print(chat_history)
 
 
